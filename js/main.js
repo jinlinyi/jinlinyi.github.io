ea3f41b3e4fe676b915c41ef7d679ff09c95b62a
jinlinyi <jinlinyi@umich.edu>
2018-12-02 23:17:30 -0500

    one page

--- js/main.js
+++ js/main.js
@@ -60,15 +60,7 @@ $(document).ready(function(){
 
 
 
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
-            $("#top-bar").removeClass("animated-header");
-        } else {
-            $(".navbar-brand a").css("color","inherit");
-            $("#top-bar").addClass("animated-header");
-        }
-    });
+   
 
     $("#clients-logo").owlCarousel({
  
